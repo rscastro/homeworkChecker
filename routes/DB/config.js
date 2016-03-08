8d163649bd78e7bbc3cc925d3d47f81101007ca1
Richard Castro <rscastro13@gmail.com>
2016-03-08 00:54:38 -0800

    pgpromise

--- routes/DB/config.js
+++ routes/DB/config.js
@@ -1,7 +1,6 @@
 //create connection here
 var bluebird = require('bluebird');
 var pgp = require('pg-promise')({promiseLib: bluebird});
-var pg = require('pg');
 if(process.env.DEPLOYED){
 	var connectionString = {
     host: 'postgres', // server name or IP address; 
@@ -11,27 +10,14 @@ if(process.env.DEPLOYED){
     password: 'mysecretpassword'
 };
 }
-else if(process.env.DATABASE_URL){
-	pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-module.exports = client;
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
-}
 else{
 var connectionString = process.env.DATABASE_URL||"postgres://localhost:5432/edmodo";
 
 }
 
 //create new db instance
-//var db = pgp(connectionString);
-//module.exports = db;
+var db = pgp(connectionString);
+module.exports = db;
 
 //to test locally you must create a database 'wayd' in postgres
 
