aa12b388406930bc430b2705ab89e46c5e7595e8
Richard Castro <rscastro13@gmail.com>
2016-03-08 00:51:09 -0800

    db test2

--- routes/DB/config.js
+++ routes/DB/config.js
@@ -15,8 +15,8 @@ else if(process.env.DATABASE_URL){
 	pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
-var db = client;
-module.exports = db;
+
+module.exports = client;
   client
     .query('SELECT table_schema,table_name FROM information_schema.tables;')
     .on('row', function(row) {
